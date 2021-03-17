167a3826a464da6df241a95c1974e6e5f59a10c0
katherinehw <71749102+katherinehw@users.noreply.github.com>
2021-03-16 23:40:03 -0700

    Create Node.java
    Creates Node Class for bst

---
+++ Node.java
@@ -0,0 +1,21 @@
+package herman;
+
+//error needed to create a separate class for Node
+
+//creates Node class with key attribute
+public class Node {
+	int key; //value held in the node
+	Node leftChild, rightChild;
+
+/**
+*  
+* @param num value of node
+*/
+
+public Node(int num){ 
+	key = num;
+	leftChild = rightChild = null; 
+    } 
+	
+
+}
